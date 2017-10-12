7da64be8579c25b90215df4f7e8790e31891096b
Hugo <hugolonima@gmail.com>
2017-10-12 12:18:20 +0200

    Primera versión

---
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -0,0 +1,14 @@
+package edu.upc.eseiaat.pma.countrylist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CountryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+        String [] countries = getResources().getStringArray(R.array.countries);
+    }
+}
